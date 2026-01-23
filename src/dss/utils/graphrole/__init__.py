5aaaaec712a80ac2b43cc618164645e2396bae34
HiddeFranke <hiddefranke@gmail.com>
2026-01-23 17:02:01 +0100

    Update __init__.py

--- src/dss/utils/graphrole/__init__.py
+++ src/dss/utils/graphrole/__init__.py
@@ -1,2 +1,5 @@
-from graphrole.features.extract import RecursiveFeatureExtractor
-from graphrole.roles.extract import RoleExtractor
+# from graphrole.features.extract import RecursiveFeatureExtractor
+# from graphrole.roles.extract import RoleExtractor
+
+from ..utils.graphrole.features.extract import RecursiveFeatureExtractor
+from ..utils.graphrole.roles.extract import RoleExtractor
